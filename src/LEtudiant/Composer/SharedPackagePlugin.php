c0d3e7471766a8561a3c1fd8b34039f1d8646ba9
cyganm <cyganm@gmail.com>
2022-07-27 10:03:52 -0500

    Update SharedPackagePlugin.php

--- src/LEtudiant/Composer/SharedPackagePlugin.php
+++ src/LEtudiant/Composer/SharedPackagePlugin.php
@@ -55,21 +55,6 @@ class SharedPackagePlugin implements PluginInterface
     public function uninstall(Composer $composer, IOInterface $io) {
 
     }
-    
-    public function download(PackageInterface $package, PackageInterface $prevPackage = null)
-    {
-    
-    }
-
-    public function prepare(string $type, PackageInterface $package, PackageInterface $prevPackage = null)
-    {
-        
-    }
-    
-    public function cleanup(string $type, PackageInterface $package, PackageInterface $prevPackage = null)
-    {
-    
-    }
 
     /**
      * @param Composer $composer
